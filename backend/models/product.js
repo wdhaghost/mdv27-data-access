c200a5024a8762317d2bcb818fdd6174aa1f5a29
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-02-03 16:49:18 +0100

    Product Model

---
+++ backend/models/product.js
@@ -0,0 +1,22 @@
+
+import db from '../config/mysql.js';
+
+export const getAllProduct = () => {
+
+};
+
+export const getProductById = (id) => {
+
+};
+
+export const createProduct = (user) => {
+
+};
+
+export const updateProduct = (id, user) => {
+
+};
+
+export const deleteProduct = (id) => {
+
+};
