f72a6119200252fa0937484fd59797b3b7de990a
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-02-04 15:25:29 +0100

    productController

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,22 +1,39 @@
 
 import db from '../config/mysql.js';
 
-export const getAllProduct = () => {
-    return 'ok';
+export const getAllProduct = (callback) => {
+    db.query('SELECT * FROM produits',callback)
 };
 
-export const getProductById = (id) => {
-
+export const getProductById = (id,callback) => {
+    db.query('SELECT * FROM produits WHERE id = ?', [id], callback);
 };
 
-export const createProduct = (user) => {
+export const createProduct = (product,callback) => {
+  const { nom, description, prix, categorie, revendeur } = product;
 
+  if (categorie && !/^[A-Za-z0-9]+$/.test(categorie)) {
+    return callback(new Error("La catégorie doit contenir uniquement des lettres et des chiffres."));
+  }
+  
+  const query = 'CALL CreateProduit(?, ?, ?, ?, ?)';
+    
+  db.query(query, [nom, description, prix, categorie.toLowerCase(), revendeur], callback);
 };
 
-export const updateProduct = (id, user) => {
+export const updateProduct = (id, product,callback) => {
+  const {id, nom, description, prix, statut } = product;
 
+  if (categorie && !/^[A-Za-z0-9]+$/.test(categorie)) {
+    return callback(new Error("La catégorie doit contenir uniquement des lettres et des chiffres."));
+  }
+  
+  const query = 'CALL ModifyProduit(?,?, ?, ?, ?)';
+    
+  db.query(query, [id,nom, description, prix,statut], callback);
+    
 };
 
-export const deleteProduct = (id) => {
-
+export const deleteProduct = (id,callback) => {
+    db.query('CALL CreateProduit(nom,descrioption,prix,categorie,revendeur',callback)
 };
