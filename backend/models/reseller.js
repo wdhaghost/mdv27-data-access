a9d77018967716efbe8942ed9072e72416484dfa
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-02-04 14:31:31 +0100

    reseller model sql

---
+++ backend/models/reseller.js
@@ -0,0 +1,22 @@
+
+import db from '../config/mysql.js';
+
+export const getAllReseller = () => {
+    db.query('SELECT * ')
+};
+
+export const getResellerById = (id) => {
+    
+};
+
+export const createReseller = (reseller) => {
+
+};
+
+export const updateReseller = (id, reseller) => {
+
+};
+
+export const deleteReseller = (id) => {
+
+};
