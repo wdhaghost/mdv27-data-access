e09d141aa79c58ce86d85bf6a8e490aa19938e6a
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-02-05 16:41:17 +0100

    changes responses for reseller controller

--- backend/controllers/resellerController.js
+++ backend/controllers/resellerController.js
@@ -11,30 +11,31 @@ export const getResellerById = (req, res) => {
   Reseller.getResellerById(req.params.id, (err, results) => {
     if (err) return res.status(500).json(err);
     if (results.length === 0)
-      return res.status(404).json({ message: 'Produit non trouvé' });
+      return res.status(404).json({ message: 'Revendeur non trouvé' });
     res.json(results[0]);
   });
 };
 
 export const createReseller = (req, res) => {
-  const Reseller = req.body;
+  const reseller = req.body;
   Reseller.createReseller(reseller, (err, results) => {
     if (err) return res.status(500).json(err);
-    res.status(201).json({ message: 'Produit créé', id: results.insertId });
+    res.status(201).json({ message: 'Revendeur créé', id: results });
   });
 };
 
 export const updateReseller = (req, res) => {
-  const Reseller = req.body;
-  Reseller.updateReseller(req.params.id, reseller, (err) => {
+  const reseller= req.body;
+  Reseller.updateReseller(reseller, (err) => {
     if (err) return res.status(500).json(err);
-    res.json({ message: 'Produit mis à jour' });
+    res.json({ message: 'Revendeur mis à jour' });
   });
 };
 
 export const deleteReseller = (req, res) => {
-  Reseller.deleteReseller(req.params.id, (err) => {
+  const reseller = req.body
+  Reseller.deleteReseller(reseller, (err) => {
     if (err) return res.status(500).json(err);
-    res.json({ message: 'Produit supprimé' });
+    res.json({ message: 'Revendeur supprimé' });
   });
 };
