94490777077dc5884e014c1c24b63c7ac905ccb7
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-02-05 16:40:53 +0100

    resellers model crud

--- backend/models/reseller.js
+++ backend/models/reseller.js
@@ -1,22 +1,37 @@
 
 import db from '../config/mysql.js';
 
-export const getAllReseller = () => {
-    db.query('SELECT * ')
+export const getAllReseller = (callback) => {
+    db.query('SELECT * from revendeurs',callback)
 };
 
-export const getResellerById = (id) => {
+export const getResellerById = (id,callback) => {
     
+    db.query('SELECT * from revendeurs WHERE revendeurs_id =? ',[id],callback)
 };
 
-export const createReseller = (reseller) => {
-
+export const createReseller = (reseller,callback) => {
+    const {name}= reseller
+    if (name && !/^[A-Za-z]+$/.test(name)) {
+      return callback(new Error("Le revendeur doit contenir uniquement des lettres."));
+    }
+    
+    const query = 'CALL CreateRevendeur(?)';
+    db.query(query, [name], callback);
 };
 
-export const updateReseller = (id, reseller) => {
-
+export const updateReseller = (reseller,callback) => {
+    const {id,name}= reseller
+    if (name && !/^[A-Za-z]+$/.test(name)) {
+      return callback(new Error("Le revendeur doit contenir uniquement des lettres."));
+    }
+    
+    const query = 'CALL ModifyRevendeur(?,?)';
+    db.query(query, [id,name], callback);
 };
 
-export const deleteReseller = (id) => {
-
+export const deleteReseller = (reseller,callback) => {
+    const {name} = reseller
+    const query = 'CALL DeleteRevendeur(?)';
+    db.query(query,[name],callback)
 };
