7379b8f402a5207ab5add1bed06a23303bbbd0ec
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-02-05 17:01:47 +0100

    router updated

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
 import * as productController from '../controllers/productController.js' 
 import * as resellerController from '../controllers/resellerController.js' 
+import * as categoryController from '../controllers/categoryControllery.js'
 
 const router = Router()
 
@@ -8,13 +9,14 @@ router.get('/products',productController.getAllProduct)
 router.get('/products/:id',productController.getProductById)
 router.post('/products',productController.createProduct)
 router.put('/products',productController.updateProduct)
-router.delete('/products',productController.deleteProduct)
 
-router.get('/reseller',resellerController.getAllProduct)
-router.get('/reseller/:id',resellerController.getProductById)
-router.post('/reseller',resellerController.createProduct)
-router.put('/reseller',resellerController.updateProduct)
-router.delete('/reseller',resellerController.deleteProduct)
+router.get('/resellers',resellerController.getAllReseller)
+router.get('/resellers/:id',resellerController.getResellerById)
+router.post('/resellers',resellerController.createReseller)
+router.put('/resellers',resellerController.updateReseller)
+router.delete('/resellers',resellerController.deleteReseller)
+
+router.get('/categories',categoryController.getAllCategories)
 
 
 
