47037e72afabf502c0f216bd9e82d42b6b8573f2
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-02-07 17:26:11 +0100

    fichier de config mysql pour le script

---
+++ service/config/mysql.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2"
+import dotenv from "dotenv"
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion MySQL:', err);
+  } else {
+    console.log('Connecté à MySQL');
+  }
+});
+
+export default connection;
